e60c2bc0d4bd5f0b65a2f8777f25e279c17c6522
Leonardo Henrique <leonardohenriquesoarasalvez784@gmail.com>
2025-07-03 21:55:08 -0300

    GET and POST

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
 
-require '../vendor/autoload.php';
+// require '../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
-var_dump($_ENV['KEY']);
+// var_dump($_ENV['KEY']);
+
+var_dump($_GET);
+var_dump($_POST);
